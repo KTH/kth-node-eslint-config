f79462775a62bb3f2c03aa12634cbfcd41fdc8c7
Alexander Urban <lexa@toxa.de>
2021-02-04 13:35:35 +0100

    Linting fixed

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   extends: [
-    "airbnb",
-    "prettier",
-    "plugin:jest-dom/recommended",
-    "plugin:testing-library/recommended",
-    "plugin:testing-library/react",
+    'airbnb',
+    'prettier',
+    'plugin:jest-dom/recommended',
+    'plugin:testing-library/recommended',
+    'plugin:testing-library/react',
   ],
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       classes: true,
@@ -19,38 +19,32 @@ module.exports = {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["prettier", "jest", "jest-dom", "testing-library"],
+  plugins: ['prettier', 'jest', 'jest-dom', 'testing-library'],
   rules: {
-    "import/extensions": "off",
-    "no-plusplus": "off",
-    "no-restricted-syntax": [
-      "error",
+    'import/extensions': 'off',
+    'no-plusplus': 'off',
+    'no-restricted-syntax': [
+      'error',
       {
-        selector: "LabeledStatement",
-        message:
-          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
       },
       {
-        selector: "WithStatement",
-        message:
-          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
       },
     ],
-    "no-underscore-dangle": "off",
-    "prefer-template": "off",
-    "prettier/prettier": ["error"],
-    "react/destructuring-assignment": [
-      "error",
-      "always",
-      { ignoreClassFields: true },
-    ],
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off",
-    "sort-imports": "off",
-    strict: "off",
-    "global-require": "off",
-    "react/no-multi-comp": "off",
+    'no-underscore-dangle': 'off',
+    'prefer-template': 'off',
+    'prettier/prettier': ['error'],
+    'react/destructuring-assignment': ['error', 'always', { ignoreClassFields: true }],
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'sort-imports': 'off',
+    strict: 'off',
+    'global-require': 'off',
+    'react/no-multi-comp': 'off',
   },
-};
+}
